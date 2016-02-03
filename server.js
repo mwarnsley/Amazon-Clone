d2e8156948f68c07ea11cd207d0de03bea8bdab1
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-03 13:38:20 -0500

    adding the node modules in order to use express that has been installed

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+//adding express to use
 var express = require('express');
 
+//using express objects
 var app = express();
 
-app.listen(3000, function(err){
+//setting the port into a variable for caching
+var port = 3000;
+
+//running the server on port 3000
+app.listen(port, function(err){
     if(err){
         throw err;
     }else{
-        console.log("Server is running");
+        console.log("Server is running on port: " + port);
     }
 });
