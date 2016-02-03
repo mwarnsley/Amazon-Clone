5d57962773765d17d79bccff56ba21afde5653fe
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-03 14:24:54 -0500

    building out the user schema for the user profile

--- models/user.js
+++ models/user.js
@@ -4,3 +4,42 @@ var mongoose = require('mongoose');
 //adding the bcrypt
 var bcrypt = require('bcrypt-nodejs');
 
+//declaring the mongoose schema
+var Schema = mongoose.Schema;
+
+//user schema attributes / characteristics / fields
+var UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    password: String,
+    profile: {
+        name: {
+            type: String,
+            default: ''
+        },
+        picture: {
+            type: String,
+            default: ''
+        },
+        address: String,
+        history: [{
+            date: Date,
+            paid: {
+                type: Number,
+                default: 0
+            }
+        }]
+    }
+});
+
+
+//hash the password before we even save it to the database
+
+
+
+
+
+//compare password in the database with the one the user types in
