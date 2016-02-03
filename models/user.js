d046fe8be4c1044fd9c66f0a6727b5ca67654d88
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-03 14:39:03 -0500

    adding the pre saving of the password before it gets to the database

--- models/user.js
+++ models/user.js
@@ -35,9 +35,19 @@ var UserSchema = new Schema({
     }
 });
 
-
 //hash the password before we even save it to the database
-
+UserSchema.pre('save', function(next){
+    var user = this;
+        if(!user.isModified('password')) return next();
+        bcrypt.genSalt(10, function(err, salt){
+            if(err) return next(err);
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+           if(err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
 
 
 
